b9678ea8796753da99cc12e92fa1194dfa7ac785
Aditya Tomar <adityatomar1803@gmail.com>
2023-04-30 20:38:43 +0530

    postgres adaptor configured- new line attempt

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,15 +8,13 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
 
-
     def test_add_numbers(self):
        
         res = calc.add(2, 3)
         self.assertEqual(res, 5)
 
-
     def test_sub_numbers(self):
        
         res = calc.subtract(5, 10)
         self.assertEqual(res, 5)
-
+    
